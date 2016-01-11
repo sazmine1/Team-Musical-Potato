2043e2b602eb4bf6685d7f27179690d90f2838eb
Sean <sbourke@stuy.edu>
2016-01-11 10:47:30 -0500

    Card.java

--- Card.java
+++ Card.java
@@ -1,3 +1,21 @@
 public class Card {
-
+    public String suit,number;
+    public Card(String suit1, String number1){
+	suit=suit1;
+	number=number1;
+    }
+    public String toString(){
+	String retStr="--------\n|"+number+"    "+number+"|";
+	retStr+="\n|"+suit+"    "+suit+"|";
+	for(int x=0;x<3;x++){
+	    retStr+="\n|      |";
+	}
+	retStr+="\n|"+number+"    "+number+"|";
+	retStr+="\n|"+suit+"    "+suit+"|";
+	return retStr;
+    }
+    public static void main(String[] args){
+	Card King = new Card("h","K");
+	System.out.println(King);
+    }
 }
